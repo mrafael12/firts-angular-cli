098c7c93a7e35cdb69c27b6df225ea8dc517595e
Marcos Rafael de Sousa Paula <rafael.paula@textiluniao.com.br>
2018-04-04 08:37:57 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { User } from './user';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Tasks';
+  
+  user: User = {
+    name: "Bob",
+    age: 25
+  }
+}
---
+++ src/app/task-list/task-list.component.ts
@@ -0,0 +1,15 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent {
+  tasks = [];
+  task = "";  
+  isAdmin = true;
+  add():void{
+    this.tasks.push(this.task);
+  }
+}
